09828bf0342322ea2862684438d55e0a3ba7a040
Thiago Esquivel <thiago.sesquivel@gmail.com>
2021-10-09 12:44:26 -0300

    Alterando scripts de inserção

--- scripts.sql
+++ scripts.sql
@@ -3,16 +3,16 @@ CREATE DATABASE inema;
 
 -- Create table clients
 CREATE TABLE inema.clients (
-    id varchar(36) NOT NULL,
+    id int(6) AUTO_INCREMENT NOT NULL,
     name varchar(255) NOT NULL,
     emaiL varchar(255) NOT NULL,
-    cpf varchar(12) NOT NULL,
+    cpf varchar(18) NOT NULL,
     PRIMARY KEY (id)
 )
 
 -- Create table products
 CREATE TABLE inema.products(
-   id varchar(36) NOT NULL,
+   id int(6) AUTO_INCREMENT NOT NULL,
    name varchar(255) NOT NULL,
    price float (8,2) NOT NULL,
    quantity int not null,
@@ -21,9 +21,8 @@ CREATE TABLE inema.products(
 
 -- Create table sales
 CREATE TABLE inema.sales(
-	 id varchar(36) NOT NULL,
-	client_id varchar(36) NOT NULL,
-    total float(8,2),
+	 id int(6) AUTO_INCREMENT NOT NULL,
+	 client_id int(6) NOT NULL,
      created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   	 updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now(),
   	PRIMARY KEY (id),
@@ -32,9 +31,9 @@ CREATE TABLE inema.sales(
 
 -- Create table sales_has_products
 CREATE TABLE inema.sales_has_products(
-    id varchar(36) NOT NULL,
-    sale_id varchar(36) NOT NULL,
-    product_id varchar(36) NOT NULL,
+    id int(6) AUTO_INCREMENT NOT NULL,
+    sale_id int(6) NOT NULL,
+    product_id int(6) NOT NULL,
     quantity int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (sale_id) REFERENCES sales(id),
@@ -43,18 +42,18 @@ CREATE TABLE inema.sales_has_products(
 
 
 -- insert in table client
-INSERT INTO inema.clients(id, name, email, cpf) values
-(UUID(),'ClientA', 'clientea@teste.com', '047.608.047-90'),
-(UUID(), 'ClientB','cliente2@teste.com', '066.282.081-92'),
-(UUID(), 'ClientC', 'client3@teste.com', '690.733.139-02'),
-(UUID(), 'ClientD', 'client4@teste.com', '382.320.268-55')
+INSERT INTO inema.clients( name, email, cpf) values
+('ClientA', 'clientea@teste.com', '047.608.047-90'),
+( 'ClientB','cliente2@teste.com', '066.282.081-92'),
+( 'ClientC', 'client3@teste.com', '690.733.139-02'),
+( 'ClientD', 'client4@teste.com', '382.320.268-55')
 
 -- insert in table products
-INSERT INTO inema.products(id, name, price, quantity) values
-(UUID(), 'ProdutoA', 8.00, 2),
-(UUID(), 'ProdutoB', 9.00, 9),
-(UUID(), 'ProdutoC', 10.00,10),
-(UUID(), 'ProdutoD', 2.00, 5),
-(UUID(), 'ProdutoE', 12.00,8),
-(UUID(), 'ProdutoF', 3.00 ,3),
-(UUID(), 'ProdutoG', 5.00 , 7)
+INSERT INTO inema.products(name, price, quantity) values
+( 'ProdutoA', 8.00, 2),
+( 'ProdutoB', 9.00, 9),
+( 'ProdutoC', 10.00,10),
+( 'ProdutoD', 2.00, 5),
+( 'ProdutoE', 12.00,8),
+( 'ProdutoF', 3.00 ,3),
+( 'ProdutoG', 5.00 , 7)
